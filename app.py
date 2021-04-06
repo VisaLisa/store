a88b8fac08e34da64f4fc26c6e7e4e22004a19ea
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-04-06 16:28:07 -0500

    update gitignore

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import stripe
+# import stripe
 
 from flask import Flask, jsonify, render_template
 from flask_debugtoolbar import DebugToolbarExtension
@@ -13,10 +13,10 @@ toolbar = DebugToolbarExtension(app)
 app.config['SECRET_KEY'] = "stuffthings"
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'postgres:///lisastore'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51IdJOfLr6dKpoHJsWZmDW1ImIVKAreVf0GhQDzfTT74uiNGeR2Pd42ZzFOHIJ4Al1PmZMU54btPNINuFbbsve5SP00ynr27tFh'
-app.config['STRIPE_SECRET_KEY'] = 'sk_test_51IdJOfLr6dKpoHJs2lQ0qg5DYKkmBSsnPlpiL393r4pN0YKqN0p1dpAf0X0VGJbjSXWjy027R7foLtBJLewEAvjq00M1PmdOqC'
+# app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51IdJOfLr6dKpoHJsWZmDW1ImIVKAreVf0GhQDzfTT74uiNGeR2Pd42ZzFOHIJ4Al1PmZMU54btPNINuFbbsve5SP00ynr27tFh'
 
-stripe.api_key = app.config['STRIPE_SECRET_KEY']
+# stripe.api_key = app.config['STRIPE_SECRET_KEY'] = 'sk_test_51IdJOfLr6dKpoHJs2lQ0qg5DYKkmBSsnPlpiL393r4pN0YKqN0p1dpAf0X0VGJbjSXWjy027R7foLtBJLewEAvjq00M1PmdOqC'
+
 
 connect_db(app)
 db.create_all()
@@ -25,29 +25,29 @@ db.create_all()
 @app.route("/")
 def homepage():
     """Show homepage - show product list"""
-    return render_template("index.html", key=stripe_keys['publishable_key'])
+    return render_template("index.html")
 
 # TODO: product page - add, delete, edit
-@app.route('/products')
-def show_all_products():
-    """Return a list of products"""
+# @app.route('/products')
+# def show_all_products():
+#     """Return a list of products"""
 
-    product_list = Product.query.all()
-    return render_template('products.html', product_list=product_list)
+#     product_list = Product.query.all()
+#     return render_template('products.html', product_list=product_list)
 
-@app.route('/product/add', methods=["GET", "POST"])
-def add_product():
+# @app.route('/product/add', methods=["GET", "POST"])
+# def add_product():
 
-    form = AddProductForm()
+#     form = AddProductForm()
 
-    # validate product
-    if form.validate_on_submit():
+#     # validate product
+#     if form.validate_on_submit():
 
 
 
 # @app.route('/product/<int:product_id>', methods=["GET", "POST"])
 # def show_product():
-"""product list page - all products"""
+# """product list page - all products"""
 
 # cart - add product to cart 
     # app.route(methods=["GET","POST"])
